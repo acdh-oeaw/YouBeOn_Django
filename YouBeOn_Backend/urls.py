e4ba843d87b57312e5980c3b35587f367e1f50ba
Markus Hasenberger <markus@hasenberger.at>
2021-05-08 09:52:09 +0200

    still the same?

--- YouBeOn_Backend/urls.py
+++ YouBeOn_Backend/urls.py
@@ -1,4 +1,4 @@
-"""YouBeOn_Backend URL Configuration
+"""youbeon_backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'YouBeOn_Backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youbeon_backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
