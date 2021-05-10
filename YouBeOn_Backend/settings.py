9294a94b56d0dc2d1cf64da34b3bfb4013b43d25
Markus Hasenberger <markus@hasenberger.at>
2021-05-10 09:23:28 +0200

    settings

--- YouBeOn_Backend/settings.py
+++ YouBeOn_Backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_api_framework',
+    'rest_framework',
     'youbeon_api.apps.YoubeonApiConfig'
 ]
 
