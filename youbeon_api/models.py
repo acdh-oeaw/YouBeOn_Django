2da8572f2d03ae196ba73bcc07ad28a347391a8e
Markus Hasenberger <markus@hasenberger.at>
2021-05-10 09:32:53 +0200

    created the models

--- youbeon_api/models.py
+++ youbeon_api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Ort(models.Model):
+    id = models.AutoField(primary_key=True)
+    bezeichnung = models.CharField(max_length=100)
+    bemerkung = models.CharField(max_length=200)
+    koordinate_b = models.CharField(max_length=50)
+    koordinate_l = models.CharField(max_length=50)
+    osm_id = models.IntegerField()
+
+class Influencer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    bemerkung = models.CharField(max_length=200)
+    location = models.CharField(max_length=100)
+    osm_id = models.IntegerField()
+    gnd = models.CharField(max_length=100)
+
+
+class Kategorie(models:Model):
+    id = id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+class Religion(models:Model):
+    id = id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+class Idee(models:Model):
+    id = id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
