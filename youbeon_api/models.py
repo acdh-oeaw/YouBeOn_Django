cfc645b942d0abb20071f8fe0548cf5499ca534c
Markus Hasenberger <markus@hasenberger.at>
2021-05-15 13:59:06 +0200

    many to Many

--- youbeon_api/models.py
+++ youbeon_api/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Ort(models.Model):
-    id = models.AutoField(primary_key=True)
-    bezeichnung = models.CharField(max_length=100)
-    bemerkung = models.CharField(max_length=200)
-    koordinate_b = models.CharField(max_length=50)
-    koordinate_l = models.CharField(max_length=50)
-    osm_id = models.IntegerField()
-
-class Influencer(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    bemerkung = models.CharField(max_length=200)
-    location = models.CharField(max_length=100)
-    osm_id = models.IntegerField()
-    gnd = models.CharField(max_length=100)
-
 class Kategorie(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
@@ -29,3 +13,24 @@ class Idee(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
 
+class Ort(models.Model):
+    id = models.AutoField(primary_key=True)
+    bezeichnung = models.CharField(max_length=100)
+    bemerkung = models.CharField(max_length=200)
+    koordinate_b = models.CharField(max_length=50)
+    koordinate_l = models.CharField(max_length=50)
+    kategorie = models.ManyToManyField(Kategorie)
+    idee = models.ManyToManyField(Idee)
+    religion = models.ManyToManyField(Religion)
+    osm_id = models.IntegerField()
+
+class Influencer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    bemerkung = models.CharField(max_length=200)
+    location = models.CharField(max_length=100)
+    kategorie = models.ManyToManyField(Kategorie)
+    idee = models.ManyToManyField(Idee)
+    religion = models.ManyToManyField(Religion)
+    osm_id = models.IntegerField()
+    gnd = models.CharField(max_length=100)
--- youbeon_api/serializer.py
+++ youbeon_api/serializer.py
@@ -8,12 +8,12 @@ from youbeon_api.models import Idee, Influencer, Kategorie, Ort, Religion
 class InfluencerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Influencer
-        fields = ('id', 'name', 'bemerkung', 'location', 'osm_id', 'gnd')
+        fields = ('id', 'name', 'bemerkung', 'location', 'osm_id', 'gnd', 'kategorie', 'idee', 'religion')
 
 class OrtSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ort
-        fields = ('id', 'bezeichnung', 'bemerkung', 'koordinate_l', 'koordinate_b', 'osm_id')
+        fields = ('id', 'bezeichnung', 'bemerkung', 'koordinate_l', 'koordinate_b', 'osm_id', 'kategorie', 'idee', 'religion')
 
 class IdeeSerializer(serializers.ModelSerializer):
     class Meta:
