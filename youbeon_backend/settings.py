19db97a5e274f7485d93a37d9be91f15aec9a302
Dalibor Pančić <dalibor.pancic@oeaw.ac.at>
2021-05-19 19:20:26 +0200

    move db to postgress

--- youbeon_backend/settings.py
+++ youbeon_backend/settings.py
@@ -105,12 +105,17 @@ WSGI_APPLICATION = 'youbeon_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db(default='sqlite:///'+os.path.join(BASE_DIR, 'db.sqlite3'))
 }
+
 
 
 # Password validation
