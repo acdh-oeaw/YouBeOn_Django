54d8ca651c9ed508956680cd6656f77c87470d49
Dalibor Pančić <dalibor.pancic@oeaw.ac.at>
2021-05-19 17:48:09 +0200

    reconfig settings

--- youbeon_backend/settings.py
+++ youbeon_backend/settings.py
@@ -11,6 +11,27 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from environ import Env
+
+def generate_secret_key(length=50):
+    sample = '1234567890-=!@#$%^&*()_+qwertyuiopasdfghjklzxcvbnm'
+    return ''.join([sample[unpack('>I', os.urandom(4))[0] % len(sample)] for i in range(length)])
+
+throw_away_key = generate_secret_key()
+
+env = Env(
+    # set casting, default value
+    DJANGO_DEBUG=(bool, False),
+    DJANGO_ALLOWED_HOSTS=(list, re.sub(r'https?://', '', os.environ.get('GITLAB_ENVIRONMENT_URL', 'http://127.0.0.1,http://localhost')).split(',')),
+    DJANGO_SECRET_KEY=(str, throw_away_key),
+    DJANGO_CORS_ORIGIN_WHITELIST=(tuple, ('127.0.0.1', 
+                                          '127.0.0.1:8080',
+                                          'localhost:8000',
+                                          'localhost:8080'))
+)
+
+Env.read_env()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +41,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l%mpzf8ske%n22sm-z1^h9v0ozab4h1_-^jh8ybi*pis^yh15('
+# SECRET_KEY = 'l%mpzf8ske%n22sm-z1^h9v0ozab4h1_-^jh8ybi*pis^yh15('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,14 +99,13 @@ WSGI_APPLICATION = 'youbeon_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
-DATABASES = env('DATABASE_URL')
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
