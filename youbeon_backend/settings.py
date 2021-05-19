1f616ac727843755da724296bba4efbedfb84cee
Dalibor Pančić <dalibor.pancic@oeaw.ac.at>
2021-05-19 18:55:25 +0200

    move django debug config to CI/CD env variables

--- youbeon_backend/settings.py
+++ youbeon_backend/settings.py
@@ -48,7 +48,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = env('DJANGO_DEBUG')
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = env('DJANGO_ALLOWED_HOSTS')
