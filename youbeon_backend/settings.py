7d493439203966c1b63d486c09304796ecf771eb
Markus Hasenberger <markus@hasenberger.at>
2021-06-28 10:43:57 +0200

    wrong placement of middleware

--- youbeon_backend/settings.py
+++ youbeon_backend/settings.py
@@ -65,10 +65,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'allow_cidr.middleware.AllowCIDRMiddleware',
