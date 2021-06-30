b2cdaf716f6bad567c03c0e1b555f46645118170
Markus Hasenberger <markus@hasenberger.at>
2021-06-30 09:30:13 +0200

    test django cors

--- youbeon_backend/settings.py
+++ youbeon_backend/settings.py
@@ -77,10 +77,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "https://app.youbeon.eu"
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+#CORS_ALLOWED_ORIGINS = [
+#    "http://localhost:8080",
+#    "http://127.0.0.1:9000",
+#    "https://app.youbeon.eu"
+#]
 
 ROOT_URLCONF = 'youbeon_backend.urls'
 
