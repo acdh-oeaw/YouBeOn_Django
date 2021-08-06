95523c7077fc515e385df7331b59e5a835a622b1
Markus Hasenberger <markus@hasenberger.at>
2021-08-06 13:06:56 +0200

    ditto

--- youbeon_api/urls.py
+++ youbeon_api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 
 from rest_framework import routers
 
-from youbeon_api.views import IdeeViewSet, KategorieViewSet, ReligionViewSet, InfluencerViewSet, OrtViewSet
+from youbeon_api.views import IdeeViewSet, KategorieViewSet, ReligionViewSet, InfluencerViewSet, OrtViewSet, IdeeDetail
 
 router = routers.DefaultRouter()
 router.register(r'idee', IdeeViewSet)
@@ -13,4 +13,5 @@ router.register(r'ort', OrtViewSet)
 
 urlpatterns = [
    path('', include(router.urls)),
+   path('idee/(?P<pk>[0-9]+)/$', IdeeViewSet.idee_detail)
 ]
--- youbeon_backend/urls.py
+++ youbeon_backend/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', include('youbeon_api.urls'))
+    path('/', include('youbeon_api.urls'))
 ]
