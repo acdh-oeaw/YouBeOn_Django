5235804fa0974426d21c61327fdf69ffddf36a3e
Markus Hasenberger <markus@hasenberger.at>
2021-08-18 11:11:03 +0200

    another try at a fix

--- youbeon_api/views.py
+++ youbeon_api/views.py
@@ -5,7 +5,6 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from collections import Counter
 from django import forms
-import django_excel as excel
 
 from youbeon_api.serializer import KategorieSerializer, IdeeSerializer, ReligionSerializer, InfluencerSerializer, OrtSerializer
 from youbeon_api.models import Kategorie, Idee, Religion, Influencer, Ort
@@ -72,29 +71,6 @@ class IdeeViewSet(viewsets.ModelViewSet):
 class ReligionViewSet(viewsets.ModelViewSet):
     queryset = Religion.objects.all()
     serializer_class = ReligionSerializer
-
-#get Data into Database
-def upload(request):
-    if request.method == "POST":
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            filehandle = request.FILES["file"]
-            return excel.make_response(
-                filehandle.get_sheet(), "csv", file_name="download"
-            )
-    else:
-        form = UploadFileForm()
-    return render(
-        request,
-        "upload_form.html",
-        {
-            "form": form,
-            "title": "Database upload",
-            "header": (
-                "Bitte wähle das Hauptfile mti den Verknüpfungen aus:"
-            ),
-        },
-    )
 
 def import_data(request):
     if request.method == "POST":
